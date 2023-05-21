433d938d880a3e9faedc4f572f8936cd4e84dda8
Yeonsu <yeonsuyam@gmail.com>
2023-05-21 17:22:00 +0900

    Connect IMU Sensor

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+void setupIMU(){
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+  Serial.begin(9600);
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+  // accel.setRange(ADXL345_RANGE_8_G);
+  // accel.setRange(ADXL345_RANGE_4_G);
+  // accel.setRange(ADXL345_RANGE_2_G);
+}
+
+void setup() {
+  setupIMU();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  Serial.println(event.acceleration.z);
+  delay(1);
 }
